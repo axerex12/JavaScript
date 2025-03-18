bc689155dc43d9ecc92868ba9a97a3cdd91accd7
axerex12 <axel.aaldijk12@gmail.com>
2025-03-18 12:52:24 +0200

    Refactor distance calculation to accept coordinates in a single input and add new HTML and JavaScript files for task 3

--- js1 recap/task2/main1.2.js
+++ js1 recap/task2/main1.2.js
@@ -1,9 +1,11 @@
 'use strict';
 
-const x1 = parseFloat(prompt('Enter x1 : '));
-const y1 = parseFloat(prompt('Enter y1 : '));
-const x2 = parseFloat(prompt('Enter x2 : '));
-const y2 = parseFloat(prompt('Enter y2 : '));
+const xy = (prompt('Enter x1,y1,x2,y2 in single input no spaces : '));
+const input = xy.split("")
+const x1 = input[0]
+const y1 = input[1]
+const x2 = input[2]
+const y2 = input[3]
 
 const distanceBetween = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
 
