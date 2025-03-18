efb7476793be5a4174b23135776847e53fd6ffc8
axerex12 <axel.aaldijk12@gmail.com>
2025-03-18 17:22:49 +0200

    Add HTML and JavaScript files for task 1 in js-recap2

---
+++ js-recap2/task1/main2.1.js
@@ -0,0 +1,14 @@
+const fruits = ["apple", "banana", "orange", "grape", "kiwi"];
+console.log(fruits);
+console.log(fruits.length);
+console.log(fruits[2]);
+console.log(fruits[fruits.length - 1]);
+const vegetables = [];
+let inputVegetable = prompt("Enter a vegetable");
+vegetables.push(inputVegetable);
+let inputVegetable2 = prompt("Enter a vegetable");
+vegetables.push(inputVegetable2);
+let inputVegetable3 = prompt("Enter a vegetable");
+vegetables.push(inputVegetable3);
+console.log(vegetables);
+console.log(vegetables.length);
