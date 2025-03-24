766cc5ef927872781720fee5191f118a9534abdf
axerex12 <axel.aaldijk12@gmail.com>
2025-03-24 11:41:29 +0200

    Add HTML, CSS, and JavaScript files for js-recap3 tasks 1 to 5 and completed task 1 in js recap3

---
+++ js-recap3/t1/t1.js
@@ -0,0 +1,44 @@
+// array for todo list
+const todoList = [
+  {
+    id: 1,
+    task: 'Learn HTML',
+    completed: true,
+  },
+  {
+    id: 2,
+    task: 'Learn CSS',
+    completed: true,
+  },
+  {
+    id: 3,
+    task: 'Learn JS',
+    completed: false,
+  },
+  {
+    id: 4,
+    task: 'Learn TypeScript',
+    completed: false,
+  },
+  {
+    id: 5,
+    task: 'Learn React',
+    completed: false,
+  },
+];
+
+// add your code here
+const html = `<li>
+<input type="checkbox" id="todo-1" checked>
+<label for="todo-1">Buy milk</label>
+</li>
+<li>
+<input type="checkbox" id="todo-2">
+<label for="todo-2">Buy eggs</label>
+</li>
+<li>
+<input type="checkbox" id="todo-3" >
+<label for="todo-3">Buy bread</label>
+</li>`
+const targetElement = document.querySelector('#subject');
+targetElement.insertAdjacentHTML('beforeend',html);
