c79c7ade385e92debe71a6154c5f67395e241919
axerex12 <axel.aaldijk12@gmail.com>
2025-03-24 12:55:21 +0200

    Add todo list functionality in js-recap3 task 2

--- js-recap3/t2/t2.js
+++ js-recap3/t2/t2.js
@@ -28,3 +28,22 @@ const todoList = [
 ];
 
 // add your code here
+const ulElement = document.querySelector('#subject');
+
+for (let i = 0; i < todoList.length; i++) {
+  const todo = todoList[i];
+
+  const newLi = document.createElement('li');
+  const newInput = document.createElement('input');
+  newInput.type = 'checkbox';
+  newInput.id = `todo-${todo.id}`;
+  newInput.checked = todo.completed;
+
+  const newLabel = document.createElement('label');
+  newLabel.htmlFor = `todo-${todo.id}`;
+  newLabel.textContent = todo.task;
+
+  newLi.appendChild(newInput);
+  newLi.appendChild(newLabel);
+  ulElement.appendChild(newLi);
+}
